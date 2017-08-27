23a24bdbd51cad63e1084def60da3923e31fe5b7
Blaine Richardson <blainerrichardson@gmail.com>
2017-08-27 02:02:24 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Dog
+
+  def intialize(name,breed)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+end
